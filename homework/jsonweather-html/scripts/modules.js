64afd1e58f6e4e63dcac75c933f080adeb8352f8
josephcanto <can16004@byui.edu>
2016-11-22 16:28:37 -0700

    update
    completed the Current Location Assessment

---
+++ homework/jsonweather-html/scripts/modules.js
@@ -0,0 +1,6 @@
+// Include modules into the html file
+$(function() {
+  $("#page-header").load("../modules/header.html");
+  $("#page-nav").load("../modules/navigation.html");
+  $("#footer-content").load("../modules/footer.html");
+});
