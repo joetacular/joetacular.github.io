73740596067622c2b419d8996671ed234d61feab
josephcanto <can16004@byui.edu>
2016-12-02 14:50:03 -0700

    update
    -removed totitlecase function or whatever it was called
    -changed all links in search.js file to https instead of http
    -removed comments in code that were unnecessary

--- scripts/search.js
+++ scripts/search.js
@@ -13,18 +13,12 @@ function getData(lat, lon) {
             var summary = data.current_observation.weather;
             var highTemp = Math.round(data.forecast.simpleforecast.forecastday["0"].high.fahrenheit);
             var lowTemp = Math.round(data.forecast.simpleforecast.forecastday["0"].low.fahrenheit);
-//            var precip = data.current_observation.precip_today_in;
-//            var feelsLike = data.current_observation.feelslike_f;
-//            var windDir = data.current_observation.wind_dir;
             var icon = data.current_observation.icon_url;
             var iconName = data.current_observation.icon;
             $('#cityDisplay').text(location);
             $('title').text(location + ' | Weather Home');
             $('#currentTemp').text(temp + "°F");
             $('#summary').text(summary);
-//            $('#add1').text("Feels Like: " + feelsLike + "°F");
-//            $('#add2').text("Precipitation: " + precip + " inches");
-//            $('#add3').text("Wind Direction: " + windDir);
             $('#highTemp').text("High: " + highTemp + "°F");
             $('#lowTemp').text("Low: " + lowTemp + "°F");
             $('#iconDiv').html("<img src=" + icon + " alt=" + iconName + ">");
@@ -36,7 +30,7 @@ function getData(lat, lon) {
 $('#query').keyup(function() {
     var value = $('#query').val();
     var rExp = new RegExp(value, "i");
-    $.getJSON("http://autocomplete.wunderground.com/aq?query=" + value + "&cb=?", function(data) {
+    $.getJSON("https://autocomplete.wunderground.com/aq?query=" + value + "&cb=?", function(data) {
         console.log(data); // test for JSON received
         // Begin building output
         var output = '<ol>';
@@ -51,13 +45,6 @@ $('#query').keyup(function() {
         $("#searchResults").html(output); // send results to the page
     }); // end getJSON
 }); // end onkeyup
-
-// A function for changing a string to TitleCase
-function toTitleCase(str) {
-    return str.replace(/\w+/g, function (txt) {
-        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    });
-}
 
 // Intercept the menu link clicks
 $("#searchResults").on("click", "a", function (evt) {
