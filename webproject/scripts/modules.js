4a200c6363289b21d69101bd78ae96b2fc29a575
josephcanto <can16004@byui.edu>
2016-12-11 00:15:26 -0700

    update
    trying to add my christmas website this way instead since the program won't sync

---
+++ webproject/scripts/modules.js
@@ -0,0 +1,5 @@
+$(function() {
+    $('#page-header').load('webproject/modules/header.html');
+    $('#page-nav').load('webproject/modules/nav.html');
+    $('#page-footer').load('webproject/modules/footer.html');
+});
